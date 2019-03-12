429c4284888c944dd43787e60e5b616e84f5b999
felipearaujos <felipe.araujo.s@live.com>
2019-03-11 23:36:14 -0300

    initial

---
+++ currencyProvider.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type QuoteResponse struct {
+	Quotes map[string]float64 `json:"quotes"`
+}
+
+func listAllCoinsAvaliableCoinsAndCurrency() QuoteResponse {
+	url := "http://www.apilayer.net/api/live?access_key=5f6b1096c7a12c66227a659e438509cc"
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var quoteRespose QuoteResponse
+	err = json.Unmarshal(responseData, &quoteRespose)
+
+	return quoteRespose
+}
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/labstack/echo"
+)
+
+func MakeHandlers() {
+	e := echo.New()
+	e.GET("/", healthCheck)
+	e.GET("/quotes", listAllCoinsAvaliableCoins)
+
+	log.Fatal(e.Start(":8081"))
+	fmt.Println("Listening:8081")
+}
+
+func healthCheck(c echo.Context) error {
+	return c.JSON(http.StatusOK, "OK")
+}
+
+func listAllCoinsAvaliableCoins(c echo.Context) error {
+	currencyAndCoinsResponse := listAllCoinsAvaliableCoinsAndCurrency()
+
+	keys := make([]string, 0, len(currencyAndCoinsResponse.Quotes))
+	for k := range currencyAndCoinsResponse.Quotes {
+		keys = append(keys, strings.Replace(k, "USD", "", -1))
+	}
+
+	return c.JSON(http.StatusOK, keys)
+}
+
+func main() {
+	MakeHandlers()
+}
