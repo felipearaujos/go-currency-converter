0248dfc5c50d6011d1a72c80f277a860fb5f1570
felipearaujos <felipe.araujo.s@live.com>
2019-03-13 23:48:18 -0300

    on the fly because i want!

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 
 	"log"
 	"net/http"
-	"strings"
 
 	"github.com/felipearaujos/go.currency.convert/repository"
 	"github.com/labstack/echo"
@@ -15,6 +14,7 @@ func MakeHandlers() {
 	e := echo.New()
 	e.GET("/", healthCheck)
 	e.GET("/quotes", listAllCoinsAvaliableCoins)
+	e.POST("/convert", convertCurrency)
 
 	log.Fatal(e.Start(":8081"))
 	fmt.Println("Listening:8081")
@@ -24,23 +24,42 @@ func healthCheck(c echo.Context) error {
 	return c.JSON(http.StatusOK, "OK")
 }
 
+type QuoteConverterRequest struct {
+	From QuoteConverterValueRequest `json:"from"`
+	To   QuoteConverterValueRequest `json:"to"`
+}
+type QuoteConverterValueRequest struct {
+	Value    float64 `json:"Value"`
+	Currency string  `json:"Currency"`
+}
+
+func convertCurrency(c echo.Context) error {
+	var converter QuoteConverterRequest
+	err := c.Bind(&converter)
+
+	if err != nil {
+		return c.JSON(http.StatusUnprocessableEntity, err.Error())
+	}
+
+	currencyAndCoinsResponse := repository.ListAllCoinsAvaliableCoinsAndCurrency()
+
+	fromCurrency := currencyAndCoinsResponse.Quotes[converter.From.Currency]
+	toCurrency := currencyAndCoinsResponse.Quotes[converter.To.Currency]
+
+	dolarFrom := converter.From.Value / fromCurrency
+	convertedValue := dolarFrom * toCurrency
+
+	return c.JSON(http.StatusOK, convertedValue)
+
+}
+
 func listAllCoinsAvaliableCoins(c echo.Context) error {
 	currencyAndCoinsResponse := repository.ListAllCoinsAvaliableCoinsAndCurrency()
 
 	keys := make([]string, 0, len(currencyAndCoinsResponse.Quotes))
 	for k := range currencyAndCoinsResponse.Quotes {
-		keys = append(keys, strings.Replace(k, "USD", "", -1))
-	}
-
-	return c.JSON(http.StatusOK, keys)
-}
-
-func list(c echo.Context) error {
-	currencyAndCoinsResponse := services.ListAllCoinsAvaliableCoinsAndCurrency()
-
-	keys := make([]string, 0, len(currencyAndCoinsResponse.Quotes))
-	for k := range currencyAndCoinsResponse.Quotes {
-		keys = append(keys, strings.Replace(k, "USD", "", -1))
+		//keys = append(keys, strings.Replace(k, "USD", "", -1))
+		keys = append(keys, k)
 	}
 
 	return c.JSON(http.StatusOK, keys)
